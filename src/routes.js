9c594733417c62d462a1a63ff8fb8b95891ce270
jtown970 <jasonrtowner@gmail.com>
2020-06-30 17:03:29 -0700

    tried changing Dash to dash in routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Switch, Route} from 'react-router-dom'
 import Auth from './components/Auth'
-import Dash from './components/Dash'
+import dash from './components/Dash'
 import UserVotes from './components/UserVotes'
 import HouseVotes from './components/HouseVotes'
 import Graph from './components/Graph'
@@ -9,7 +9,7 @@ import Graph from './components/Graph'
 export default (
   <Switch>
     <Route exact path="/" component={Auth}/>
-    <Route path="/dash" component={Dash}/>
+    <Route path="/dash" component={dash}/>
     <Route path="/user/votes" component={UserVotes}/>
     <Route path="/house/votes" component={HouseVotes}/>
     <Route path="/graph" component={Graph}/>
