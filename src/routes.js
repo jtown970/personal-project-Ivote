1ab3e4c59928a8abfc7f5fec58e9cbe2dc0aaed3
jtown970 <jasonrtowner@gmail.com>
2020-06-30 17:06:31 -0700

    changed the Dash to dash in components

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Switch, Route} from 'react-router-dom'
 import Auth from './components/Auth'
-import dash from './components/Dash'
+import dash from './components/dash'
 import UserVotes from './components/UserVotes'
 import HouseVotes from './components/HouseVotes'
 import Graph from './components/Graph'
